a1160ed1bef837782cd042a804e8a729eca19d8a
ginnoyong <yongginno@gmail.com>
2024-10-10 23:12:52 +0800

    0.2

--- agent_functions/crew.py
+++ agent_functions/crew.py
@@ -21,56 +21,130 @@ client = OpenAI(api_key=OPENAI_KEY)
 # Set the base URL of a website, e.g., "https://example.com/", so that the tool can search for sub-pages on that website
 
 from crewai_tools import WebsiteSearchTool
-tool_websearch = WebsiteSearchTool("https://www.moe.gov.sg/post-secondary/admissions")
+tool_admission_websearch = WebsiteSearchTool("https://www.moe.gov.sg/post-secondary/admissions",
+                                             temperature=0)
+tool_ecg_websearch = WebsiteSearchTool("https://www.moe.gov.sg/coursefinder",
+                                             temperature=0)
+
 
 # Agents
-agent_admission = Agent(
-    role="Admission Advisor",
+agent_friendly_advisor = Agent(
+    role="Post-Secondary Education Advisor",
 
-    goal="Provide clear and targetted advice on post secondary school admission criteria and procedure specific to the client as specified by his/her input: {topic}.",
+    goal="Consolidate the responses provided by the other agents, \
+        and rewrite a coherent, helpful and succinct final response to the user's query.",
 
-    backstory="""You are an expert in Singapore's Minstry of Education Post Secondary School registration and admission matters.
-    You will read and understand the admission criteria, procedures and timelines for various post secondary school education options
-    and their respective applicable admission exercises from the Minstry of Education's website using the task tool. \
-    You will then analyze the input provided by the client: '{topic}', \
-    and advise the client helpfully and factually. Provide all the important information that he/she needs to know to be successful in the application.
-    If you are providing procedure steps in your response, make them clear and easy to follow with key timeline to take note of.
-    Make use of the tool provided to source for answers to questions on Singapore's Minstry of Education Post Secondary School registration and admission matters.""",
+    backstory=f"""You are a superb communicator whose field of expertise is in Singapore's Post-Secondary School Education. 
+    Your job is to consolidate the information in the responses provided by the other agents, \
+        and communicate information that is coherent and only relevant to the user's query. 
+        You will also craft your resonse in such a way that is easy for a Post Secondary School student to understand. 
+        The vast majority of Post Secondary School students are between 16 and 17 years old. 
+    """,
 
     allow_delegation=False, # we will explain more about this later
 
-	  verbose=True, # to allow the agent to print out the steps it is taking
+	verbose=True, # to allow the agent to print out the steps it is taking
+)
 
-    tools=[tool_websearch],
+agent_admission = Agent(
+    role="Admission Advisor",
+
+    goal="Provide clear and targetted advice on post secondary school education admission criteria and procedure \
+        specific to the user's query: {topic_admission}.",
+
+    backstory="""You are an expert in Singapore's Minstry of Education Post Secondary School registration and admission matters.
+    You have the capability to read and understand all admission matters on post secondary school education and the various admission exercises, \
+        such as admission criteria, procedures and timelines. 
+    You are also capable of analyze the user's query and provide a factual and useful response. 
+    Your work will be the basis for the Post-Secondary Education Advisor to craft the final response to the user's query.
+    """,
+
+    allow_delegation=False, # we will explain more about this later
+
+	verbose=True, # to allow the agent to print out the steps it is taking
+)
+
+agent_ecg = Agent(
+    role="Education and Career Advisor",
+
+    goal="Perform Values, Interests, Personality, Skills (V.I.P.S.) analysis of the user based on the information \
+        provided by the user in his/her query: {topic_ecg}.\
+        Shortlist 5 Post-Secondary School Education courses/schools that are most suitable to the user based on the V.I.P.S analysis.",
+
+    backstory="""You are an expert in identifying Post-Secondary School Education courses/schools that best-match the V.I.P.S. analysis of students.\
+          Singapore's Minstry of Education Post Secondary School registration and admission matters.
+          Your work will be the basis for the Post-Secondary Education Advisor to craft the final response to the user's query.
+          """,
+
+    allow_delegation=False, # we will explain more about this later
+
+	verbose=True, # to allow the agent to print out the steps it is taking
+
+    tools=[tool_ecg_websearch],
 )
 
 # Tasks
-task_admission = Task(
+task_craft_response = Task(
     description="""\
-    1. Understand all the admission exercises detailed in MOE website.
-    2. Identify the suitable admission exercise(s) based on the inputs provided by the client: {topic}.
-    3. Provide useful and factual answer(s) to the client based on the identified admission exercise(s).
-    4. Do not provide information on any admission exercises that the client is ineligible for or which timeline has already passed.
-    5. If the client is asking for the next step(s) in the process or what he/she needs to do next, \
-    provide useful and simple-to-follow step-by-step guide and important timeline with checkboxes on how to proceed in the admission procedure.
-    6. If the input is generic and does not provide enough information for you to provide useful, practical response(s) regarding post-secondary school admission exercise(s), \
-    ask the user for more specific information.
-    7. If the information provided about the client in the input does not meet the criteria of all admission exercises, \
-    be encouraging and provide practical advice what the client can do for further education. Provide helpful timeline.
-    8. Always provide the exact url links to the source of information as reference to the client for each point of suggestion or advice or guide you provide in your response.
+    1. Analyse the work produced by the other agents 
+    2. Consolidate and compile the work provided by the other agents in a logical, coherent and meaningful way. 
+    3. From the output of the previous step, craft your response using ONLY information that is applicable to the user based on his/her query. 
+    4. Craft your response using this information in a manner that is succinct and easy to follow, suitable for Post-Secondary School students.
     """,
 
     expected_output="""\
-    Useful and easy to understand information about suitable Post-Secondary school admission exercise(s) based on the user's question/prompt. \
-    Include clear instructions for the next step(s) in the application / registration procedure where required.""",
+    Factual, useful and coherent information/advice only relevant to the user's query on matters about Post-Secondary Education \
+        crafted in a way that is succinct and easy to understand.
+    """,
+
+    agent=agent_friendly_advisor,
+
+    # tools=[tool_admission_websearch],
+)
+
+task_admission = Task(
+    description="""\
+    1. Understand all admission matters on Post Secondary School Education.
+    2. Analyse information provided by the user in his/her query: {topic_admission}.
+    3. Respond factually and meaningfully to the user. 
+    4. If the user is asking about eligibility, respond with only information of admission exercises and options that the user is eligible for.\
+    Ask for more information if the user did not provide enough information for you to answer the query.  
+    5. Check the timeline(s). Highlight if and when the eligible admission exercise(s) is/are open. 
+    6. Include the url link(s) to the webpage(s) where the information in your response are sourced from.
+    """,
+
+    expected_output="""\
+    Factual, useful and succinct information/advice on matters about Post-Secondary Education admission exercises in response to the user's query.
+    """,
 
     agent=agent_admission,
+
+    tools=[tool_admission_websearch],
+)
+
+# Tasks
+task_ecg = Task(
+    description="""\
+    1. Perform Values, Interests, Personality, Skills (V.I.P.S.) analysis of the user based on the information \
+        provided by the user in his/her query: {topic_ecg}.
+    2. Shortlist not more than 5 Post-Secondary Education only courses/schools suitable for the user based on the analysis.
+    3. Do NOT include University courses.
+    3. Rank the shortlisted courses/schools with the most suitable one first and the least suitable last. 
+    4. Include the url link(s) to the webpage(s) of the identified courses/schools in your list.
+    """,
+
+    expected_output="""\
+    A list 5 Post-Secondary Education only courses/schools suitable for the user based on the V.I.P.S. analysis. \
+        """,
+
+    agent=agent_ecg,
+
 )
 
 # Crew
 crew = Crew(
-    agents=[agent_admission],
-    tasks=[task_admission],
+    agents=[agent_admission, agent_ecg, agent_friendly_advisor],
+    tasks=[],
     verbose=True
 )
 
@@ -78,7 +152,21 @@ crew = Crew(
 from datetime import date
 # Kickoff
 # Start the crew's task execution
-def crew_kickoff(user_message):
+def crew_kickoff(dict_component_queries):
     today = date.today()
-    result = crew.kickoff(inputs={"topic": f"{user_message} Today's date is {today}"})
+    result = crew.kickoff(inputs={"topic_admission": f"{dict_component_queries.get('Admission')}. Today's date is {today}",
+                                  "topic_ecg":f"{dict_component_queries.get('ECG')}"})
     return result
+
+def let_the_agents_handle_it(dict_component_queries):
+    if "ECG" in dict_component_queries.keys():
+        crew.tasks.append(task_ecg)
+    if "Admission" in dict_component_queries.keys():
+        # crew.agents.append(agent_admission)
+        crew.tasks.append(task_admission)
+    if len(crew.tasks)==0:
+        response = ""
+    else:
+        crew.tasks.append(task_craft_response)
+        response = crew_kickoff(dict_component_queries).raw
+    return response
--- logics/query_handler.py
+++ logics/query_handler.py
@@ -1,12 +1,12 @@
 from helper_functions.llm import get_completion_by_messages
-from agent_functions.crew import crew_kickoff
-
+from agent_functions.crew import let_the_agents_handle_it
+import json
 
 def query_categorizer(incoming_message):
     system_message = f"""
         You are an AI bot tasked with pre-processing messages sent to a chatbot designed to assist user's 
         queries regarding Post-Secondary School Education in Singapore. 
-        Your task is to categorize user's query into the following categories:
+        Your task is to categorize user's query into one or more of the following categories:
         'Admission': If the user is asking about Post-Secondary School Education admission matters, \
             such as procedure, timeline, eligibility criteria, minimum entry requirement (MER) scores, etc.
         'CourseSchoolFinder': If the user is asking about specific schools or courses, such as \
@@ -15,7 +15,13 @@ def query_categorizer(incoming_message):
         'ECG': If the user is asking for guidance in selecting the right Post-Secondary School Education \
             school or course based on the user's interests, passion, school subjects that he/she is good at, etc.
         'Other': If the user's query doesn't fall into any of the above categories.
-        Categorize the user's query delimited by <incoming-message> into one or more categories. Output the categories in a JSON object only.
+        The user's query is delimited by <incoming-message>.
+        Step 1: Analyze if the user's query is made up of one or more component queries.
+        Step 2: Split the user's query into its component queries. Rewrite each component query into a clear and concise standalone query.
+        Step 3: Categorize each component query into one or more of the above categories.
+        Step 4: Output unique categories only. Combine component queries of the same category.  
+        Step 5: Output the categories in a JSON object only with the following format: \
+            {{category 1:combined component queries of category 1, category 2:combined component queries of category 1, ..}}
         """
     messages =  [
         {'role':'system', 'content': system_message},
@@ -23,6 +29,7 @@ def query_categorizer(incoming_message):
     ]
 
     response = get_completion_by_messages(messages)
+    return response
 
 def improve_query(user_message):
     delimiter = "#####"
@@ -85,6 +92,11 @@ def malicious_check(user_message):
 def query_handler(user_message):
     #improved_user_query = improve_query(user_message)
     #helpful_response = crew_kickoff(improved_user_query)
-    helpful_response = query_categorizer(user_message)
+    categorized_query = query_categorizer(user_message)
+    dict_categorized_query = json.loads(categorized_query)
+    # print(list(dict_categorized_query.keys()))
+    # print(list(dict_categorized_query.values()))
+    helpful_response = let_the_agents_handle_it(dict_categorized_query)
+    print(dict_categorized_query)
     return helpful_response
 
